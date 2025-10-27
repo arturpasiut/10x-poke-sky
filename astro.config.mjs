c85c8c81c7af7e04ad051f323290669f3bbe031e
ArturTHOC <artur.pasiut@thoc.dev>
2025-10-27 11:41:08 +0100

    PS-24 add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,11 @@ export default defineConfig({
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
   adapter: cloudflare(),
 });
